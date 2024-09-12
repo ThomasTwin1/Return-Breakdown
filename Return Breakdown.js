b9198dba4a55c8cf81387b6f0464b14fc2025eb3
ThomasTwin1 <sherrard.a.thomas4@gmail.com>
2024-09-12 04:13:44 -0400

    Add files via upload

---
+++ Return Breakdown.js
@@ -0,0 +1,8 @@
+function sega(){
+    var tails = "Sonic's Best Friend ";
+    return tails;
+    
+}
+var bestfriend = sega(); // For storing the result
+console.log(bestfriend); // To display the result
+console.log(sega()); // To display the result without storing it
